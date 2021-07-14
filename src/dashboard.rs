906bd14b2c5aa033964631cd3b6268087328fbc2
Gokul2406 <gokulpbharathan89@gmail.com>
2021-07-14 16:26:04 +0530

    Added options menu

---
+++ src/dashboard.rs
@@ -0,0 +1,12 @@
+pub mod dashboard_options {
+use std::io;
+    pub fn option() {
+        let mut selected_option = String::new();
+        println!("1) Create Password \n2) Delete Password \n3) View Password");
+        println!("\nSelect an option");
+        io::stdin()
+            .read_line(&mut selected_option)
+            .expect("An error occured ");
+
+    }
+}
