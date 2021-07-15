2946f95993fde529dc9baaa874bcf2ba73df0e5a
Gokul2406 <gokulpbharathan89@gmail.com>
2021-07-15 16:15:09 +0530

    Added func to create a master password

--- src/initial_setup.rs
+++ src/initial_setup.rs
@@ -1,6 +1,8 @@
 pub mod initial_setup {
     use home::home_dir;
-    use std::fs::File;
+    use std::fs::{File, write};
+    use serde_json;
+    use std::io;
 
     // The function checks if the passwords.json file exists
     // and if it doesn't, it creates the file
@@ -10,13 +12,23 @@ pub mod initial_setup {
         let password_file = password_dir.join("passwords.json");
 
         // Create the program dir where we can store the passwords file
-        if !password_dir.exists() {
-            std::fs::create_dir_all(&password_dir).unwrap();
-        }
+         std::fs::create_dir_all(&password_dir).unwrap();
 
         // Create the file where we can store the passwords
-        if !password_file.exists() {
             File::create(&password_file).unwrap();
-        }
+
+        let master_password = get_master_password_from_user().unwrap();
+        write(&password_file, serde_json::to_string_pretty(&master_password).unwrap()).unwrap();
+    }
+
+    fn get_master_password_from_user() -> Result<String, io::Error> {
+        let mut master_password = String::new();
+        println!("Lets setup your new password for this app");
+        io::stdin()
+            .read_line(&mut master_password)
+            .expect("Some error occurred");
+        let master_password = String::from(master_password.trim());
+
+        Ok(master_password)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ mod initial_setup;
 
 fn main() {
     // The inital setup covers creating the file for passwords etc.
-    initial_setup::initial_setup::check_if_file_exists();
+    let path_to_dir = home::home_dir().unwrap().join(".local/share/passafe");
+    let path_to_file = path_to_dir.join("passwords.json");
+
+    if !path_to_file.exists() {
+        println!("no file");
+        initial_setup::initial_setup::check_if_file_exists();
+    }
     dashboard::dashboard_options::list_option();
 }
